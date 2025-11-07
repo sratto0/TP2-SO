10d449e08806d444732bc0607f103176a929331b
Sofía Ratto <sratto@itba.edu.ar>
2025-11-07 14:45:42 -0300

    ps bien

--- .bash_history
+++ .bash_history
@@ -1,27 +1,3 @@
-cd Toolchain
-make
-cd ..
-make
-make
-exit
-cd root
-cd Toolchain
-make
-cd ..
-make
-make
-make
-make
-gdb
-exit
-cd root
-cd Toolchain
-make
-cd ..
-make
-make clean
-cd Toolchain
-make clean
 make
 cd ..
 make 
@@ -497,4 +473,28 @@ exit
 cd root
 make clean
 make
+exit
+cd root
+make clean
+cd Toolchain
+make
+cd ..
+make
+exit
+cd root
+make clean
+cd Toolchain
+make
+cd ..
+make
+exit
+cd root
+make clean
+cd Toolchain
+make
+cd ..
+make
+exit
+cd root
+make clean
 exit
--- Kernel/process/process.c
+++ Kernel/process/process.c
@@ -22,10 +22,16 @@ process_t *my_create_process(int64_t pid, entry_point_t entry_point,
     return NULL;
   }
 
-  process_t *proc = memory_alloc(sizeof(process_t));
+  process_t *proc = (process_t *)memory_alloc(sizeof(process_t));
 
   if (proc == NULL) {
     return NULL;
+  }
+
+  memset(proc, 0, sizeof(*proc));
+  if(name){
+    my_strncpy(proc->name, name, MAX_NAME_LEN - 1);
+    proc->name[MAX_NAME_LEN - 1] = 0;
   }
 
   proc->pid = pid;
@@ -54,8 +60,8 @@ process_t *my_create_process(int64_t pid, entry_point_t entry_point,
 
   proc->argc = count_from_argv(proc->argv);
 
-  my_strncpy(proc->name, name, sizeof(proc->name));
-  proc->name[MAX_NAME_LEN - 1] = '\0';
+  // my_strncpy(proc->name, name, sizeof(proc->name));
+  // proc->name[MAX_NAME_LEN - 1] = '\0';
 
   proc->stack_pointer = set_stack_frame(&process_caller, entry_point,
                                         proc->stack_pointer, proc->argv);
