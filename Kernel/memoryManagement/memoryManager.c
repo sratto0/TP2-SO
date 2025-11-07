5013cdc2074af562bbc32d402e92cb1fd04d403d
Sofía Ratto <sratto@itba.edu.ar>
2025-11-07 19:42:01 -0300

    pvs(falta en el makefile)

--- Kernel/memoryManagement/memoryManager.c
+++ Kernel/memoryManagement/memoryManager.c
@@ -1,3 +1,7 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java:
+// https://pvs-studio.com
+
 // #include "memoryManager.h"
 // #define BLOCK_HEADER_SIZE sizeof(block_t)
 // #define ALIGN4(x) (((((x) - 1) >> 2) << 2) + 4)
--- Userland/SampleCodeModule/commands/shellCommands.c
+++ Userland/SampleCodeModule/commands/shellCommands.c
@@ -1,3 +1,7 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java:
+// https://pvs-studio.com
+
 #include "../include/shellCommands.h"
 #include "../include/stdio.h"
 #include "../include/stdlib.h"
